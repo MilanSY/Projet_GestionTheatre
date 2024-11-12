63dc7b026f841af34cf60ded4a1c624aa64da9e1
Noah <noah60ise@gmail.com>
2024-11-12 12:51:04 +0100

    Introduction du développement pour mettre a jour une pices de théatre
    Développment du back end commancé, il faut sécuriser la requête et commancé le front

--- UtilisateurBLL/GestionTheatres.cs
+++ UtilisateurBLL/GestionTheatres.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheatreDAL;
+
 
 namespace TheatreBLL
 {
     public class GestionTheatres
     {
-        // add remove update getter setter des pièczq de theatre
+        private static GestionTheatres uneGestionTheatres; // objet BLL
+            // add remove update getter setter des pièces de theatre
+            public static void UpdateTheatre(string nom, string adresse, string ville, string codePostal, string telephone, string courriel, string siteWeb, string description)
+            {
+               TheatreDAO.UpdateTheatre(nom, adresse, ville, codePostal, telephone, courriel, siteWeb, description);
+            }
     }
 }
--- UtilisateursDAL/TheatreDAO.cs
+++ UtilisateursDAL/TheatreDAO.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace TheatreDAL
 {
-    internal class TheatreDAO
+    public class TheatreDAO
     {
+        public static void UpdateTheatre(string nom, string adresse, string ville, string codePostal, string telephone, string courriel, string siteWeb, string description)
+        {
+            string connectionString = "your_connection_string_here";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Theatre SET Nom = @nom, Adresse = @adresse, Ville = @ville, CodePostal = @codePostal, Telephone = @telephone, Courriel = @courriel, SiteWeb = @siteWeb, Description = @description WHERE Nom = @nom";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@nom", nom);
+                command.Parameters.AddWithValue("@adresse", adresse);
+                command.Parameters.AddWithValue("@ville", ville);
+                command.Parameters.AddWithValue("@codePostal", codePostal);
+                command.Parameters.AddWithValue("@telephone", telephone);
+                command.Parameters.AddWithValue("@courriel", courriel);
+                command.Parameters.AddWithValue("@siteWeb", siteWeb);
+                command.Parameters.AddWithValue("@description", description);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
