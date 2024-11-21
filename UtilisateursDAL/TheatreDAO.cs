bbcdcf317f3fe0dcf98ee249184bfdbf4b9f8d59
Noah <noah60ise@gmail.com>
2024-11-21 13:56:25 +0100

    Windows form ModifierPiècesDeThéâtre
    Finalisation du windows form permettant de modifier les informations des pièces de théâtre (Non fonctionnel)

--- UtilisateursDAL/TheatreDAO.cs
+++ UtilisateursDAL/TheatreDAO.cs
@@ -15,7 +15,87 @@ namespace TheatreDAL
         // Accesseur en lecture, renvoi une instance
         public static TheatreDAO GetUnTheatre()
         {
+<<<<<<< Updated upstream
             if (unTheatreDAO == null)
+=======
+            string connectionString = ConnexionBD.GetConnexionBD().GetchaineConnexion();
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            string query =  @"
+                UPDATE Pieces
+                SET pie_nom = @nom,
+                    pie_prix = @prix,
+                    pie_descrip = @description,
+                    pie_duree = @duree,
+                    pie_comp = @compagnieId,
+                    pie_pub = @publicId,
+                    pie_the = @themeId,
+                    pie_aut = @auteurId
+                WHERE pie_id = @id";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            //using (SqlCommand command = new SqlCommand(query, connection)) -- >  pas de ça (chat gpt...)
+            
+            // Ajout des paramètres
+            command.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.Int) { Value = id });
+            command.Parameters.Add(new SqlParameter("@nom", System.Data.SqlDbType.NVarChar) { Value = nom });
+            command.Parameters.Add(new SqlParameter("@prix", System.Data.SqlDbType.Float) { Value = prix });
+            command.Parameters.Add(new SqlParameter("@description", System.Data.SqlDbType.NVarChar) { Value = description ?? (object)DBNull.Value });
+            command.Parameters.Add(new SqlParameter("@duree", System.Data.SqlDbType.Int) { Value = duree ?? (object)DBNull.Value });
+            //faire une fonction GestionCompanie.getCompanyIdByName(nom)
+            command.Parameters.Add(new SqlParameter("@compagnieId", System.Data.SqlDbType.Int) { Value = compagnie });
+            //faire une fonction GestionPublic.getPublicIdByName(type)
+            command.Parameters.Add(new SqlParameter("@publicId", System.Data.SqlDbType.Int) { Value = publicCateg });
+            //faire une fonction GestionTheme.getThemeIdByName(nom)
+            command.Parameters.Add(new SqlParameter("@themeId", System.Data.SqlDbType.Int) { Value = theme });
+            //faire une fonction GestionAueur.getAuteurIdByNames(prenom, nom)
+            command.Parameters.Add(new SqlParameter("@auteurId", System.Data.SqlDbType.Int) { Value = (auteurPrenom,auteurNom) });
+
+            // Exécution de la commande
+            command.ExecuteNonQuery();
+            
+            // Connection.Close();
+
+        }
+
+        public static List<Theatre> GetTheatres()
+        {
+            int id;
+            string nom, description, compagnie, publicCateg, theme, auteur;
+            float prix;
+            int? duree;
+
+            string connectionString = ConnexionBD.GetConnexionBD().GetchaineConnexion();
+            SqlConnection connection = new SqlConnection(connectionString) ;
+
+
+            // Création d'une liste vide d'objets Theatre
+            List<Theatre> listTheatres = new List<Theatre>();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"
+                SELECT p.pie_id, p.pie_nom,
+                    p.pie_prix,
+                    p.pie_descrip,
+                    p.pie_duree,
+                    c.comp_nom AS compagnie,
+                    pu.pub_categ AS publicCateg, 
+                    t.the_nom AS theme, 
+                    CONCAT(a.aut_prenom, ' ', a.aut_nom) AS auteur 
+                FROM Pieces p 
+                LEFT JOIN Compagnies c ON p.pie_comp = c.comp_id 
+                LEFT JOIN Publics pu ON p.pie_pub = pu.pub_id 
+                LEFT JOIN Theme t ON p.pie_the = t.the_id 
+                LEFT JOIN Auteur a ON p.pie_aut = a.aut_id;";
+            connection.Open();
+
+            SqlDataReader monReader = cmd.ExecuteReader();
+
+
+            // Remplissage de la liste
+            while (monReader.Read())
+>>>>>>> Stashed changes
             {
                 unTheatreDAO = new TheatreDAO();
             }
